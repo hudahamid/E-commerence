a0452221af9ca863de482a0d877328d9fecd4e3c
hudahamid <huda.00091581@gmail.com>
2024-01-18 20:57:16 +0300

    jquery of product

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -2,7 +2,12 @@
 <html lang="en">
   <head>
     <!-- Required meta tags -->
+    
     @include('admin.css')
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
   </head>
   <body>
     <div class="container-scroller">
@@ -13,6 +18,12 @@
         <!-- partial -->
         @include('admin.body')
     <!-- container-scroller -->
+    
+    <!-- Bootstrap JS and jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.8/dist/umd/popper.min.js" integrity="sha384-M6N9xdQzAGHo7k5cQsMxK1PdGEddIbKWjr5z8NajSHQ5fPFSj9bGJYoGJ2ZdEdAj" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8sh+WyDE8vFQy5lFOnhZ2gCbVDBXuPktWbNIq6" crossorigin="anonymous"></script>
+
     <!-- plugins:js -->
     @include('admin.script')
     <!-- End custom js for this page -->
